40273df5895b4f1698c218b9ccfdc38832f12b2a
DoDo <dodobest01@gmail.com>
2020-09-06 20:18:37 +0800

    2 file

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,115 @@
+require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
+import Recipe from "./model/Recipe";
+import List from "./model/list";
+import * as listView from "./view/listView";
+import {
+  renderRecipe,
+  clearRecipe,
+  highlightSelectRecipe,
+} from "./view/recipeView";
+/* 
+web app tuluv
+-hailtin query , ur dun
+-tuhain uzuulj baigaa jor
+-Like joruud 
+-zahialj baigaa joriin nairlaga
+*/
 
-let search = new Search("pasta");
+const state = {};
 
-search.doSearch().then((r) => console.log(r));
+/*
+hailtiin controller = MVC
+*/
+const controlSearch = async () => {
+  // 1) web ees tulhuur ug avna
+  const quere = searchView.getInput();
+
+  if (quere) {
+    // 2) shine hailtin object uusgene
+    state.search = new Search(quere);
+    // 3) Hailt hiihed zoruulj delgetsee beldene
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
+    // 4) Hailtig guitsetgene
+    await state.search.doSearch();
+    // 5) Haitin ur dung delgetsend gargana
+    clearLoader();
+    if (state.search.result === undefined) alert("Хайлтаар илэрцгүй...");
+    else searchView.renderRecipes(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+  //
+  e.preventDefault();
+  controlSearch();
+});
+
+elements.pageButtons.addEventListener("click", (e) => {
+  //
+  //
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const gotoPageNumber = parseInt(btn.dataset.goto, 10);
+    searchView.clearSearchResult();
+    searchView.renderRecipes(state.search.result, gotoPageNumber);
+  }
+});
+
+// jor controller
+const controlRecipe = async () => {
+  // url aas id avna
+  const id = window.location.hash.replace("#", "");
+
+  if (id) {
+    // joriin model uusgene
+    state.recipe = new Recipe(id);
+    // ui delgets beldene
+
+    clearRecipe();
+    renderLoader(elements.recipeDiv);
+    highlightSelectRecipe(id);
+    // joroo tataj abchirna
+    await state.recipe.getRecipe();
+    clearLoader();
+    // jorig guitsetgeh hugatsaa
+    state.recipe.calcTime();
+    state.recipe.calcHuniiToo();
+    // joroo delgetsend gargana
+    renderRecipe(state.recipe);
+  }
+};
+
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+["hashchange", "load"].forEach((e) =>
+  window.addEventListener(e, controlRecipe)
+);
+
+/*
+ nairlaga controller
+ */
+const controlList = () => {
+  // nairlaga model uusgene
+  state.list = new List();
+  // tsverleh
+  listView.clearItems();
+
+  // ug model ruu odoo haragdaj
+  state.recipe.ingredients.forEach((n) => {
+    state.list.addItem(n);
+    // console.log(n);
+    listView.renderItem(n);
+  });
+};
+
+elements.recipeDiv.addEventListener("click", (e) => {
+  //
+  if (e.target.matches(".recipe__btn, .recipe__btn *")) {
+    controlList();
+  }
+});
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -1,9 +1,64 @@
-var superAdd = (a, b) => {
-  return a + b + 1000;
-};
+import { elements } from "./base";
 
-export const add = (a, b) => {
-  return superAdd(a, b);
+// private function
+const renderRecipe = (recipe) => {
+  const markup = `
+  <li>
+  <a class="results__link" href="#${recipe.recipe_id}">
+      <figure class="results__fig">
+          <img src="${recipe.image_url}" alt="Test">
+      </figure>
+      <div class="results__data">
+          <h4 class="results__name">${recipe.title}</h4>
+          <p class="results__author">${recipe.publisher}</p>
+      </div>
+  </a>
+  </li>`;
+  // ul ruu nemne
+  elements.searchResultList.insertAdjacentHTML("beforeend", markup);
 };
-export let multiply = (a, b) => a * b;
-export const id = 25;
+export const clearSearchQuery = () => {
+  elements.searchInput.value = "";
+};
+export const clearSearchResult = () => {
+  elements.pageButtons.innerHTML = "";
+  elements.searchResultList.innerHTML = "";
+};
+export const getInput = () => {
+  return elements.searchInput.value;
+};
+export const renderRecipes = (recipes, currenttPage = 1, resPerPage = 10) => {
+  // hailtiin ur dun
+  const start = (currenttPage - 1) * resPerPage;
+  const end = currenttPage * resPerPage;
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // huudaslalt tobch
+  const totalPages = Math.ceil(recipes.length / resPerPage);
+  renderButtons(currenttPage, totalPages);
+};
+const createButton = (page, type, direction) => {
+  return `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+  <span>Хуудас ${page}</span>
+  <svg class="search__icon">
+    <use href="img/icons.svg#icon-triangle-${direction}"></use>
+  </svg>
+  
+</button>`;
+};
+const renderButtons = (currenttPage, totalPages) => {
+  let buttonHtml;
+
+  if (currenttPage === 1 && totalPages > 1) {
+    //
+    buttonHtml = createButton(2, "next", "right");
+  } else if (currenttPage < totalPages) {
+    // umnuh bolon daraachinn huudas
+    buttonHtml = createButton(currenttPage - 1, "prev", "left");
+    buttonHtml += createButton(currenttPage + 1, "next", "right");
+  } else if (currenttPage === totalPages) {
+    // hamgiin suuliin huudas
+    buttonHtml = createButton(currenttPage - 1, "prev", "left");
+  }
+  elements.pageButtons.insertAdjacentHTML("afterbegin", buttonHtml);
+};
